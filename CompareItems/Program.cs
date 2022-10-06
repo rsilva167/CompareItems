d5f72f3518522e543e0a9a893582e4dc019ae555
Renan Santos <dosantosrenansilva@gmail.com>
2022-10-06 20:47:18 -0300

    This program was made using generics to improve the program quality.

---
+++ CompareItems/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompareItems.Entities;
+using CompareItems.Services;
+using System.Globalization;
+
+namespace CompareItems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+        }
+    }
+}
